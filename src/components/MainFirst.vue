56754870edd67e38fdea94c10969418809acd7e8
peosoow <ws71144712@daum.net>
2023-04-12 18:06:57 +0900

    [슬라이드 작업중]

--- src/components/MainFirst.vue
+++ src/components/MainFirst.vue
@@ -1,74 +1,90 @@
 <template>
-  <div class="slide">
+  <div>
+    <!-- :autoplay="{ delay: 2000, disableOnInteraction: true, pauseOnMouseEnter: true, }"  -->
      <swiper
-      :pagination="{
-        type: 'fraction',
-      }"
-      :navigation="true"
+      :pagination="{type: 'fraction'}"
+      :navigation="{ prevEl: '.swiper-button-prev', nextEl: '.swiper-button-next' }"
+      :loop="true"
+      :autoplay="{ delay: 2000, }" 
       :modules="modules"
       class="mySwiper"
     >
-      <swiper-slide>Slide 1</swiper-slide>
-      <swiper-slide>Slide 7</swiper-slide>
-      <swiper-slide>Slide 8</swiper-slide>
-      <swiper-slide>Slide 9</swiper-slide>
+      <swiper-slide v-for="e in 6" :key="e">
+        <img :src="require(`@/assets/images/main_visual_swipe${e}.png`)" :alt="`배너`">
+      </swiper-slide>
+      
+      <div class="swiper-button-prev"></div>
+      <div class="swiper-button-next"></div>
+      <div class="swiper-pagination"></div>
     </swiper>
+    <!-- <div>
+      <button >시작</button>
+      <button >중지</button>
+    </div> -->
   </div>
 </template>
 
 <script>
 // Import Swiper Vue.js components
-import { Swiper, SwiperSlide } from 'swiper/vue';
+import { Swiper, SwiperSlide, } from 'swiper/vue';
 
 // Import Swiper styles
-
+import 'swiper/css'
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
 
 // import required modules
-import { Pagination, Navigation } from 'swiper';
+import { Pagination, Navigation, Autoplay } from 'swiper';
 
   export default {
     name: 'MainFirst',
      components: {
-    Swiper,
-    SwiperSlide,
+      Swiper,
+      SwiperSlide,
   },
   setup() {
     return {
-      modules: [Pagination, Navigation],
+      modules: [Pagination, Navigation, Autoplay]
     };
+  },
+  data() {
+    return {
+    }
   },
   }
 </script>
 
 <style lang="scss" scoped>
-.slide{
+.mySwiper{
   position: relative;
   height: 690px;
-}
-.swiper {
-  width: 500px;
-  height: 500px;
+  .swiper {
+    width: 100%;
+    height: 100%;
+  }
+  
+  .swiper-slide {
+    text-align: center;
+    font-size: 18px;
+    background: #fff;
+  
+    /* Center slide text vertically */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  
+  .swiper-slide img {
+    display: block;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+  .swiper-button-next, .swiper-button-prev{
+    color: white;
+  }
+
 }
 
-.swiper-slide {
-  width: inherit;
-  text-align: center;
-  font-size: 18px;
-  background: #fff;
-
-  /* Center slide text vertically */
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.swiper-slide img {
-  display: block;
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-}
 </style>
